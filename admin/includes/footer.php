1fc0825c1de39571ffce28ae1757d065d203e331
“Jakaria <jakariaszlzmm12@gmail.com>
2022-02-10 22:00:28 +0600

    LMS Code Updated

---
+++ admin/includes/footer.php
@@ -0,0 +1,11 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2018 BITC Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : Jakaria Emon</a> 
+                <p> <span class="glyphicon glyphicon-envelope"></span> jakariaemon1521@gmail.com</p> 
+                  </div>
+
+            </div>
+        </div>
+    </section>
---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2018 BITC Library Management System |<a href="https://www.facebook.com/profile.php?id=100004882938464" target="_blank"  > Designed by : Jakaria Emon</a>
+ <p> <span class="glyphicon glyphicon-envelope"></span> jakariaemon1521@gmail.com</p> 
+                </div>
+
+            </div>
+        </div>
+    </section>
